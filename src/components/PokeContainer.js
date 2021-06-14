3cc497d3da6a8246bca9cbb2fae8718d675d9b40
William Parra <w3webservices.net@gmail.com>
2021-06-14 09:26:00 -0400

    se eliminaron dependecias innecesarias

--- src/components/PokeContainer.js
+++ src/components/PokeContainer.js
@@ -1,4 +1,4 @@
-import React, { useState, /*useMemo,*/ useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import PokeCard from './PokeCard'
 import Loader from './Loader'
